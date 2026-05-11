cd89b4fbccf8f59a69c458db81599c27b51685f2
Iasia <iasiacooper3@gmail.com>
2026-05-11 16:26:43 -0400

    Add WebDriverWait to CartPage for CI stability

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -2,10 +2,15 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CartPage {
 
     private final WebDriver driver;
+    private final WebDriverWait wait;
 
     //Locators
     private final By checkoutButton = By.id("checkout");
@@ -13,9 +18,10 @@ public class CartPage {
 
     public CartPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void clickOnCheckoutButton() {
-        driver.findElement(checkoutButton).click();
+        wait.until(ExpectedConditions.elementToBeClickable(checkoutButton)).click();
     }
 }
