7b1eaa6fb2bf83140e61e5325ebc7fb09b7ba7d7
Iasia <iasiacooper3@gmail.com>
2026-09-16 13:54:29 -0400

    Updated inventorypage

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -47,26 +47,18 @@ public class InventoryPage {
 
 
     public void goToCart() {
-        WebElement cart = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(cartLink)
+        wait.until(
+                ExpectedConditions.elementToBeClickable(cartLink)
+        ).click();
+
+        wait.until(
+                ExpectedConditions.urlContains("cart.html")
         );
-
-        ((JavascriptExecutor) driver).executeScript(
-                "arguments[0].scrollIntoView({block: 'center'});",
-                cart
-        );
-
-        wait.until(ExpectedConditions.elementToBeClickable(cart));
-
-        System.out.println("Cart link found.");
-        System.out.println("Cart link displayed: " + cart.isDisplayed());
-        System.out.println("Cart link enabled: " + cart.isEnabled());
-
-        cart.click();
-
-        wait.until(ExpectedConditions.urlContains("cart.html"));
 
         System.out.println("Cart page loaded: " + driver.getCurrentUrl());
+
+
+
 
     }
 
